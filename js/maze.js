adebce0d5ab48fcaa1459f19701f49526b2714b1
Gregory Ndukwu <ndukwusud@gmail.com>
2022-01-24 01:33:38 -0600

    te

---
+++ js/maze.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    var $boundary = $(".boundary");
+    var $status = $("#status");
+    var $start = $("#start");
+    var $end = $("#end");
+    var $check = 0;
+    
+    StartGame();
+
+    function StartGame() {
+
+        $start.on("click", function () {
+            
+            $boundary.removeClass("youlose");
+            $status.text("Game Start");
+            checkGame();
+            endGame();
+               
+        })
+
+    }
+
+    function endGame() {
+
+        $end.mouseenter(function () {
+            $status.text("Congratulation You Win!!");
+           
+        })
+
+    }
+    
+    function checkGame(){
+        
+        $boundary.mouseenter(function(){
+        $boundary.addClass("youlose");
+        $(".boundary.example").addClass("youlose");
+        //$boundary.css("background-color","#ff8888;");
+        $status.text("Sorry, You Lose");
+        })
+
+    }
+
+
+})   
