e1e19a99cd4218a870df0dcc9fd281940d07784b
Gregory Ndukwu <ndukwusud@gmail.com>
2022-01-26 09:57:48 -0600

    te

--- js/practice.js
+++ js/practice.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-
+/*
  var p = $("p").text();
  var tamanho = p.length + 1 ;
  console.log(tamanho);
  var texto = p.substr(0,tamanho) + "" + "YES";
 
- $("p").text(texto);
+ $("p").text(texto); */
+
+ $("body").append("<div><h1>JQuery Core</h1></div>");
 
 
 }) 
